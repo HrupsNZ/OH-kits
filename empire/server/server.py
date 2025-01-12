af01d3a3add9612e33469479519d058109e6b732
Cx01N <anthony.rose@bc-security.org>
2025-01-12 11:33:44 -0500

    added launcher.txt to prevent compilation failure

--- empire/server/server.py
+++ empire/server/server.py
@@ -86,13 +86,14 @@ def reset():
         f"{CSHARP_DIR_BASE}/Data/Tasks/CSharp/Compiled/netcoreapp3.0"
     )
 
+    file_util.clear_file_contents(f"{CSHARP_DIR_BASE}/Data/EmbeddedResources/launcher.txt")
+
     if os.path.exists(empire_config.starkiller.directory):
         shutil.rmtree(empire_config.starkiller.directory)
 
     file_util.remove_file("data/sessions.csv")
     file_util.remove_file("data/credentials.csv")
     file_util.remove_file("data/master.log")
-
 
 def shutdown_handler(signum, frame):
     """
